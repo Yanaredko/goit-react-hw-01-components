deb05df3cb6933b3af9023b6c9974746ea7cdd2e
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-09-09 21:37:46 +0200

    last task without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import user from '../data/user.json'; 
 import data from '../data/data.json'; 
 import friends from '../data/friends.json'; 
+import transactions from '../data/transactions.json'; 
 
 import Profile from './Profile.jsx'; 
 import Statistics from './Statistics.jsx';
 import FriendList from './FriendList.jsx';
+import TransactionHistory from './TransactionHistory.jsx';
 
 const App = () => {
   return (
@@ -21,6 +23,7 @@ const App = () => {
 
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
