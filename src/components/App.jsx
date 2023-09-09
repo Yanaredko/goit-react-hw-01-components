6d1940925a60b4958c696577bd87e312d807619b
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-09-09 20:52:09 +0200

    first task without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import React from 'react';
+import user from '../data/user.json'; 
+
+import Profile from './Profile.jsx'; 
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>User Profile</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;
