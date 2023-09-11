3a602cde00e2a3356fbdc3ea5933feff1761500e
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-09-11 21:13:39 +0200

    changes

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../components/styles/TransactionHistory.scss';
+// import '../components/styles/TransactionHistory.scss';
 
 const TransactionHistory = ({ items }) => {
   return (
